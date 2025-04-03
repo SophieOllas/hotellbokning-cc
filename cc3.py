2d409c08b58d9c455053bcda83eb5462a541561f
SophieOllas <ollassop@arcada.fi>
2025-04-03 09:00:59 +0000

    cc5 lektion 5

--- cc3.py
+++ cc3.py
@@ -6,26 +6,10 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from datetime import date
 
 PORT=8352
-
-# Load environment variables from .env file
-load_dotenv()
-DB_URL = os.getenv("DB_URL")
-
-# Connect to the PostgreSQL database
-conn = psycopg.connect(
-    DB_URL,
-    autocommit=True,
-    row_factory=dict_row
-)
-
-@app.get("/temp")
-def temp():
-    with conn.cursor() as cur:
-        cur.execute("SELECT * FROM hotel_rooms")
-        rows = cur.fetchall()
-    return rows
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
@@ -38,18 +22,83 @@ if __name__ == "__main__":
         ssl_keyfile="/etc/letsencrypt/privkey.pem",
         ssl_certfile="/etc/letsencrypt/fullchain.pem",
     )
+# Load environment variables from .env file
+load_dotenv()
+DB_URL = os.getenv("DB_URL")
 
-# rum nummer, pris, hur många personer
-rooms = [
-    {"room_number": 101, "price": 1200, "capacity": 2},
-    {"room_number": 102, "price": 1500, "capacity": 3},
-    {"room_number": 103, "price": 1000, "capacity": 1},
-    {"room_number": 104, "price": 2000, "capacity": 4},
-]
+# Connect to the PostgreSQL database
+conn = psycopg.connect(
+    DB_URL,
+    autocommit=True,
+    row_factory=dict_row
+)
+
+class Booking(BaseModel):
+    guest_id: int
+    room_id: int
+    datefrom: date
+    dateto: date
+
+@app.post("/bookings")
+def create_booking(booking: Booking):
+    with conn.cursor() as cur:
+        cur.execute("""INSERT INTO hotel_bookings(
+            guest_id,
+            room_id,
+            datefrom,
+            dateto
+        ) VALUES (
+            %s, 
+            %s,
+            %s,
+            %s
+            )
+            RETURNING id
+        """, [
+            booking.guest_id, 
+            booking.room_id, 
+            booking.datefrom, 
+            booking.dateto
+            ])
+        new_id = cur.fetchone()['id']
+
+    return {"msg": "Booking created successfully", "id": new_id}
+
+@app.get("/temp/{}")
+def temp():
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM hotel_rooms")
+        rows = cur.fetchall()
+    return rows
+
+
+#@app.get("/if/{user_input}")
+#def if_test(user_input: str):
+ #   message = None
+  #  if user_input == "hello" or user_input == "hi":
+   #     message = user_input + " yourself!"
+    #    elif user_input == "goodbye" and 1 > 0:
+     #       message = "goodbye yourself!"
+      #      else:
+       #         message = f"I don't understand {user_input}" # f string
+                # jämför js `I dont understand ${user_input}`;
+    #return {"msg": message}
 
 @app.get("/rooms")
-def getRooms(request: Request):
-    return rooms
+def getRooms():
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM hotel_rooms ORDER BY room_number")
+        rooms = cur.fetchall()
+        return rooms
+
+@app.get("/rooms/{id}")
+def getOneRoom(id: int):
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM hotel_rooms WHERE id = %s", [id])    
+        room = cur.fetchone()
+        if not room:
+            return {"error": "Room not found"}
+        return room
 
 # uvicorn cc3:app --host 0.0.0.0 --port 8352 --reload
 
